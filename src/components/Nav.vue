ee173e3c0e6023be5639de126515a310190cb06f
mindongjun <petanerd@gmail.com>
2018-05-15 01:46:51 +0900

    navbar change

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,18 @@
 <template>
-  <nav class="navbar navbar-light bg-light justify-content-between fixed-top">
+<div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between fixed-top">
     <a class="navbar-brand" href="#">연합아이디어톤</a>
-    <div class="navbar-nav">
-      <a class="nav-link" @click="logout()">마이페이지</a>
-      <a class="nav-link" @click="createidea()">아이디어 만들기</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" @click="createidea()">아이디어 만들기</a>
+        <a class="nav-item nav-link" @click="logout()">마이페이지</a>
+      </div>
     </div>
   </nav>
+</div>
 </template>
 <script>
 import auth from '../auth'
