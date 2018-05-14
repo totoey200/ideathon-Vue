af43a6e3bbf0ebc2ef8306b5e32e9cd181d2ff94
mindongjun <petanerd@gmail.com>
2018-05-14 23:30:17 +0900

    change card to list

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,22 @@
 <template>
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" :src='img_url' alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">{{title}}</h5>
-            <p class="card-text">{{content}}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="list">
+        <div class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-sm-flex justify-content-around">
+                <div class="img-parent">
+                    <img class="img-locate" :src='img_url' alt="Card image cap">
+                </div>
+                <div class="align-self-center">
+                        <h5 class="m-0">{{title}}ㄹ</h5>
+                    </div>
+                <div class="align-self-center">
+                    <p class="mb-1">{{content}}</p>
+                </div >
+                <div class="align-self-center">
+                    <a href="#" class="btn btn-primary">투표하기</a>
+                </div>
+                <small>3 vote</small>
+            </div>
+            <small>team name</small>
         </div>
     </div>
 </template>
@@ -16,8 +28,22 @@ export default {
 }
 </script>
 <style>
-.card{
+.list{
     display: inline-block;
     margin: 1%;
+}
+.img-parent{
+    width: 15%;
+}
+.img-locate{
+    max-width: 100%;
+    height: auto;
+}
+@media (min-width: 300px) {
+    .img-locate{
+    max-width: 100%;
+    height: auto;
+    margin: 0 auto;
+  }
 }
 </style>
