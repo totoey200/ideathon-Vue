e9f4894086ca72c6555a87269be36f9e95dd937a
mindongjun <petanerd@gmail.com>
2018-05-17 01:27:21 +0900

    add vote message

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -3,26 +3,45 @@
     <div>
         <h1>투표페이지</h1>
         <div class="mb-3">
-        <span>투표 됬는지 안됬는지 메세지 뿅뿅</span>
+        <span>{{msg}}</span>
         </div>
         <h2>본인이 맞는지 확인하세요!</h2>
-        <div class="mb-3">
+        <div>
             <p>이름름</p>
             <p>학교대학교</p>
             <p>팀이름름</p>
         </div>
     </div>
     <div class="align-self-center">
-        <a href="#" class="btn btn-primary">투표하기</a>
+        <a href="#" class="btn btn-primary" @click="voteNow()">투표하기</a>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Vote',
-  props: []
+  props: [],
+  data() {
+    return{
+      msg: ''
+    }
+  },
+  methods: {
+    voteNow () {
+    
+    axios.post('http://localhost:3000/api/idea/vote/now').then((response) => {
+      this.msg = response.response.data.message
+    },
+    (response) => {
+      this.msg = response.response.data.message
+    })
+   }
+  }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
